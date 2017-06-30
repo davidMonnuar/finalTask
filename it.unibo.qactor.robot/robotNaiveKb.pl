7c25231a3be4fa3de9352e461f2c56e0867ebfcb
David Monnuar <dmitrij.padalino@gmail.com>
2017-06-30 16:48:34 +0200

    delete robot

--- it.unibo.qactor.robot/robotNaiveKb.pl
+++
@@ -1,8 +0,0 @@
-%==============================================
-% USER DEFINED
-% system configuration: robotNaiveKb.pl
-%==============================================
-context( ctxrobot, "localhost",  "TCP", "8010" ).
-
-qactor("mock", ctxrobot).
-qactor("alarmSource", ctxrobot).
--- it.unibo.qactor.robot/src/it/unibo/qactor/robot/IRobotTimedCommand.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.qactor.robot;
-import it.unibo.iot.models.commands.baseRobot.IBaseRobotCommand;
- 
-
-public interface IRobotTimedCommand extends IBaseRobotCommand{
-	public IBaseRobotCommand getRobotBaseCommand();
-	public long getDuration();
-	public String getCompletionEventId();
-}
