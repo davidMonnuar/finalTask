c95320646ff4fef3e073fa30f0882a701ae4b7ec
Jay-P <jean.picard.45000@gmail.com>
2017-07-10 21:26:14 +0200

    Add files via upload

---
+++ it.unibo.console/srcMore/it/unibo/cxt_paSonar/pasys.pl
@@ -0,0 +1,16 @@
+%====================================================================================
+% Context cxt_paSonar  SYSTEM-configuration: file it.unibo.cxt_paSonar.paSys.pl 
+%====================================================================================
+context(cxt_parobot, "localhost",  "TCP", "8079" ).  		 
+context(cxt_paremoteconsole, "localhost",  "TCP", "8089" ).  		 
+context(cxt_pasonar, "localhost",  "TCP", "8099" ).  		 
+%%% -------------------------------------------
+qactor( qapaconsole , cxt_paremoteconsole, "it.unibo.qapaconsole.MsgHandle_Qapaconsole"   ). %%store msgs 
+qactor( qapaconsole_ctrl , cxt_paremoteconsole, "it.unibo.qapaconsole.Qapaconsole"   ). %%control-driven 
+qactor( qapasonar , cxt_pasonar, "it.unibo.qapasonar.MsgHandle_Qapasonar"   ). %%store msgs 
+qactor( qapasonar_ctrl , cxt_pasonar, "it.unibo.qapasonar.Qapasonar"   ). %%control-driven 
+%%% -------------------------------------------
+%%% -------------------------------------------
+qactor( qrparobot , cxt_parobot, "it.unibo.qrparobot.MsgHandle_Qrparobot" ). 
+qactor( qrparobot_ctrl , cxt_parobot, "it.unibo.qrparobot.Qrparobot" ). 
+
